59fc0932cd7ce3144437aa7e648658ad8c864d32
Iordache Catalin <iordache.catalin1986@gmail.com>
2022-07-04 23:41:56 +0300

    Am reusit sa fac codul

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 addEventListener(`change`, (e) => {
   const select = e.target;
   const desc = select.selectedOptions[0].text;
-  const array0 = [select.selectedOptions[0].text];
-  console.log(array0);
-});
+  const p = "Please Select";
+ if (!arr.includes(desc) && !arr.includes(p) ) {
+    arr.push(desc);
+  }
+  });
 
-const arr = ["c", "a", "d"];
+var arr = [];
 
 function Random(arr, num) {
+  alert(arr);
   document.getElementById("Rezultatul").style.display = "block"; //insert text in div
 
   const shuffled = [...arr].sort(() => 0.5 - Math.random()); // random mix
 
+  var x = shuffled.slice(0, num);
+  document.getElementById("Rezultatul").innerHTML = x;
+  makeTable(arr);
   return shuffled.slice(0, num);
+}
+
+function makeTable(array) {
+  var table = document.createElement("table");
+  var utc = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+
+  for (var i = 0; i < array.length; i++) {
+    var row = document.createElement("tr");
+    for (var j = 0; j < array[i].length; j++) {
+      var cell = document.createElement("td");
+      cell.textContent = array[i][j];
+      row.appendChild(cell);
+    }
+
+    var cell = document.createElement("td");
+    cell.textContent = utc;
+    row.appendChild(cell);
+    table.appendChild(row);
+  }
+  document.getElementById(`tbody`).append(table);
 }
