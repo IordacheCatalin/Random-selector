cc000f57daabbaa2bdc94f5ad187e8e04b5fc259
Iordache Catalin <iordache.catalin1986@gmail.com>
2022-07-04 12:39:52 +0300

    Test

---
+++ app.js
@@ -0,0 +1,68 @@
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume2");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume3");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume4");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume5");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume6");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume7");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+
+addEventListener(`change`, () => {
+  const areaSelect = document.querySelector("#Nume8");
+  //const value = select.value; => daca vreau si Value
+  const desc = areaSelect.selectedOptions[0].text;
+  const array0 = [areaSelect.selectedOptions[0].text]
+  console.log(desc , array0);
+});
+
+const array3 = [array0];
+console.log(array3);
